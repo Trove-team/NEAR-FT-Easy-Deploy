4f7f4eae2cceaed0ea10b753bcac918b68e1b56f
Trove Labs <103082550+Trove-team@users.noreply.github.com>
2024-02-21 17:45:54 -0800

    Delete init.bat

--- init.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM Run near login to login with your master account
-set NEAR_ENV=mainnet
-set TOKEN_CONTRACT=ftv2.nekotoken.near
-set OWNER_ACCOUNT=vesting-test.near
-set VESTING_CONTRACT=trovevestingtestnear.near
-
-near call %VESTING_CONTRACT% new "{\"owner\": \"%OWNER_ACCOUNT%\", \"token_contract\": \"%TOKEN_CONTRACT%\"}" --accountId %OWNER_ACCOUNT%
